8d13e01712c7b1bb3372963ea6f4873c69058692
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-10-16 10:05:52 +0200

    users endpoints

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,7 @@
 from database import engine
 from fastapi import FastAPI
 import models
-from routers import auth, todos, admin
+from routers import auth, todos, admin, users
 
 
 
@@ -13,4 +13,5 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
+app.include_router(users.router)
 
