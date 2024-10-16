73feafd4fa1ff803c6ca0dc054a187f3385c8415
Emmanuel LASTRA <emmanuellastra@gmail.com>
2024-10-16 09:33:49 +0200

    add admin router endpoints

--- TodoApp/routers/admin.py
+++ TodoApp/routers/admin.py
@@ -37,3 +37,19 @@ async def read_all(user: user_dependency, db: db_dependency):
             raise HTTPException(status_code=401, detail="User not authenticated")
     
         return db.query(Todo).all()
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int =Path(gt=0)):
+     
+        if user is None or user.get("role") != "admin":
+            raise HTTPException(status_code=401, detail="User not authenticated")
+        
+        todo_model = db.query(Todo).filter(Todo.id == todo_id).first()
+        if todo_model is None:
+            raise HTTPException(status_code=404, detail="Todo item not found")
+        
+        db.query(Todo).filter(Todo.id == todo_id).delete()
+        db.commit()
+  
+
